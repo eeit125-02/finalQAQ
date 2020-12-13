4fa4f9cd64fefe2c744d8892cf24c175707c3a31
JOYHSU <joyturtleyeah6@gmail.com>
2020-12-13 17:36:08 +0800

    加入兩個Bean
    PostBean
    CommandBean

--- BookWeb/src/main/java/com/web/book/model/PostBean.java
+++ BookWeb/src/main/java/com/web/book/model/PostBean.java
@@ -1,50 +1,96 @@
 package com.web.book.model;
-import java.io.Serializable;
 
-import javax.persistence.Column;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="NOVEL_ARTICLE")
+@Table(name="Post")
 public class PostBean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	int ID;
-	@Column(name="Title")
-	String article_title;
-	@Column(name="Content")
-	String article_content;
+	Integer post_id;
+	String post_title;
+	String post_content;
+	Integer mb_id;
+	Timestamp post_time;
 	
+	@OneToMany(mappedBy="postBean", cascade=CascadeType.ALL)
+	private Set<PostBean> posts=new LinkedHashSet<>( );
+
+	public PostBean() {}
 	
-	public PostBean() {
-	}
-	
-	public PostBean(String article_title, String article_content) {
-		this.article_title = article_title;
-		this.article_content = article_content;
-	}
-
-	public String getArticle_title() {
-		return article_title;
-	}
-
-	public void setArticle_title(String article_title) {
-		this.article_title = article_title;
+	public PostBean(Integer post_id, String post_title, String post_content, Integer mb_id, Timestamp post_time,
+			Set<PostBean> posts) {
+		super();
+		this.post_id = post_id;
+		this.post_title = post_title;
+		this.post_content = post_content;
+		this.mb_id = mb_id;
+		this.post_time = post_time;
+		this.posts = posts;
 	}
 
-	public String getArticle_content() {
-		return article_content;
+	public Integer getPost_id() {
+		return post_id;
 	}
 
-	public void setArticle_content(String article_content) {
-		this.article_content = article_content;
+	public void setPost_id(Integer post_id) {
+		this.post_id = post_id;
 	}
+
+	public String getPost_title() {
+		return post_title;
+	}
+
+	public void setPost_title(String post_title) {
+		this.post_title = post_title;
+	}
+
+	public String getPost_content() {
+		return post_content;
+	}
+
+	public void setPost_content(String post_content) {
+		this.post_content = post_content;
+	}
+
+	public Integer getMb_id() {
+		return mb_id;
+	}
+
+	public void setMb_id(Integer mb_id) {
+		this.mb_id = mb_id;
+	}
+
+	public Timestamp getPost_time() {
+		return post_time;
+	}
+
+	public void setPost_time(Timestamp post_time) {
+		this.post_time = post_time;
+	}
+
+	public Set<PostBean> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(Set<PostBean> posts) {
+		this.posts = posts;
+	}
+
 	
 	
 }
