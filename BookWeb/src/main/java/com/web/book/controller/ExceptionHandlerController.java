726513f308c1e51e6195f445fc3e483dc832d362
LAPTOP-0FNLI78A\DingDing <lsm-0317@hotmail.com>
2021-01-23 12:41:28 +0800

    錯誤攔截解除註解

--- BookWeb/src/main/java/com/web/book/controller/ExceptionHandlerController.java
+++ BookWeb/src/main/java/com/web/book/controller/ExceptionHandlerController.java
@@ -1,10 +1,11 @@
 package com.web.book.controller;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 //攔截錯誤
-//@ControllerAdvice
+@ControllerAdvice
 public class ExceptionHandlerController {
 
     @ExceptionHandler(Throwable.class)
